44aeca6d7cd4c9a998cbe10007344517e6048572
한혜경 <hyegyeong310@gmail.com>
2019-11-13 11:14:56 +0900

    [Add] sign up, login, logout request action 추가

--- ch1/front/reducers/user.js
+++ ch1/front/reducers/user.js
@@ -1,5 +1,5 @@
 const dummyUser = {
-  nickname: 'betty',
+  nickname: "betty",
   Post: [],
   Followings: [],
   Followers: []
@@ -11,31 +11,49 @@ export const initialState = {
   signUpData: null
 };
 
-export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
-export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
-export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
+export const SIGN_UP_REQUEST = "SIGN_UP_REQUEST"; // 비동기 요청 보내는 애들. 비동기 요청은 액션이 3개 나옴
+export const SIGN_UP_SUCCESS = "SIGN_UP_SUCCESS";
+export const SIGN_UP_FAILURE = "SIGN_UP_FAILURE";
 
-export const LOG_IN_REQUEST = 'LOG_IN_REQUEST';
-export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
-export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
+export const LOG_IN_REQUEST = "LOG_IN_REQUEST";
+export const LOG_IN_SUCCESS = "LOG_IN_SUCCESS";
+export const LOG_IN_FAILURE = "LOG_IN_FAILURE";
 
-export const LOG_OUT_REQUEST = 'LOG_OUT_REQUEST';
-export const LOG_OUT_SUCCESS = 'LOG_OUT_SUCCESS';
-export const LOG_OUT_FAILURE = 'LOG_OUT_FAILURE';
+export const LOG_OUT_REQUEST = "LOG_OUT_REQUEST";
+export const LOG_OUT_SUCCESS = "LOG_OUT_SUCCESS";
+export const LOG_OUT_FAILURE = "LOG_OUT_FAILURE";
 
-export const loginAction = {
-  type: LOG_IN_REQUEST
-};
-export const logoutAction = {
-  type: LOG_OUT_REQUEST
-};
+// 동기 요청은 액션이 1개. ex) INCREMENT_NUMBER
+
 export const signUpAction = data => ({
   type: SIGN_UP_REQUEST,
   data
 });
+export const signUpSuccess = {
+  type: SIGN_UP_SUCCESS
+};
+export const signUpFailure = {
+  type: SIGN_UP_FAILURE
+};
 
+export const loginAction = data => {
+  type: LOG_IN_REQUEST, data;
+};
 export const loginSucess = {
   type: LOG_IN_SUCCESS
+};
+export const loginFailure = {
+  type: LOG_IN_FAILURE
+};
+
+export const logoutAction = data => {
+  type: LOG_OUT_REQUEST, data;
+};
+export const logoutSuccess = {
+  type: LOG_OUT_SUCCESS
+};
+export const logoutFailure = {
+  type: LOG_OUT_FAILURE
 };
 
 const reducer = (state = initialState, action) => {
