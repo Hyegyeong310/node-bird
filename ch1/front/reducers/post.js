340a5679695a95eca66d58bbed5a6d062fdd1e37
한혜경 <hyegyeong310@gmail.com>
2019-11-13 19:21:09 +0900

    [Add] post comment add 기능 추가

--- ch1/front/reducers/post.js
+++ ch1/front/reducers/post.js
@@ -1,27 +1,44 @@
 export const initialState = {
   mainPosts: [
     {
+      id: 1,
       User: {
         id: 1,
         nickname: "베티"
       },
       content: "first post",
       img:
-        "https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+        "https://images.unsplash.com/photo-1513360371669-4adf3dd7dff8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      Comments: []
     } // 화면에 보일 포스트들
   ],
   imagePaths: [], // 미리보기 이미지 경로
   addPostErrorReason: "", // 포스트 업로드 실패 사유
   isAddingPost: false, //포스트 업로드 중
-  postAdded: false // 포스트 업로드 성공
+  postAdded: false, // 포스트 업로드 성공
+  isAddingComment: false,
+  addCommentErrorReason: "",
+  commentAdded: false
 };
 
 const dummyPost = {
+  id: 2,
   User: {
     id: 1,
     nickname: "betty"
   },
-  content: "나는 더미입니다."
+  content: "나는 더미입니다.",
+  Comments: []
+};
+
+const dummyComment = {
+  id: 1,
+  User: {
+    id: 1,
+    nickname: "bbb"
+  },
+  createdAt: new Date(),
+  content: "더미 댓글입니다."
 };
 
 // 메인 포스트 로딩 액션
@@ -112,6 +129,34 @@ const reducer = (state = initialState, action) => {
         ...state,
         isAddingPost: false,
         addPostErrorReason: action.error
+      };
+
+    case ADD_COMMENT_REQUEST:
+      return {
+        ...state,
+        isAddingComment: true,
+        addCommentErrorReason: "",
+        commentAdded: false
+      };
+    case ADD_COMMENT_SUCCESS:
+      const postIndex = state.mainPosts.findIndex(
+        v => v.id === action.data.postId
+      );
+      const post = state.mainPosts[postIndex];
+      const Comments = [...post.Comments, dummyComment];
+      const mainPosts = [...state.mainPosts];
+      mainPosts[postIndex] = { ...post, Comments };
+      return {
+        ...state,
+        isAddingComment: false,
+        mainPosts,
+        commentAdded: true
+      };
+    case ADD_COMMENT_FAILURE:
+      return {
+        ...state,
+        isAddingComment: false,
+        addCommentErrorReason: action.error
       };
     default:
       return state;
--- ch1/front/sagas/post.js
+++ ch1/front/sagas/post.js
@@ -1,12 +1,20 @@
-import { all, delay, fork, takeLatest, put } from "redux-saga/effects";
+import { all, call, delay, fork, takeLatest, put } from "redux-saga/effects";
 import {
   ADD_POST_REQUEST,
   ADD_POST_SUCCESS,
-  ADD_POST_FAILURE
+  ADD_POST_FAILURE,
+  ADD_COMMENT_SUCCESS,
+  ADD_COMMENT_FAILURE,
+  ADD_COMMENT_REQUEST
 } from "../reducers/post";
+
+function addPostAPI() {
+  console.log("addPostAPI");
+}
 
 function* addPost() {
   try {
+    yield call(addPostAPI);
     yield delay(2000);
     yield put({
       type: ADD_POST_SUCCESS
@@ -23,6 +31,32 @@ function* watchAddPost() {
   yield takeLatest(ADD_POST_REQUEST, addPost); // 게시글은 여러번 클릭해도 1번만 게시되어야 한다.
 }
 
+function addCommentAPI() {
+  console.log("addCommentAPI");
+}
+
+function* addComment(action) {
+  try {
+    yield call(addCommentAPI);
+    yield delay(2000);
+    yield put({
+      type: ADD_COMMENT_SUCCESS,
+      data: {
+        postId: action.data.postId
+      }
+    });
+  } catch (e) {
+    yield put({
+      type: ADD_COMMENT_FAILURE,
+      error: e
+    });
+  }
+}
+
+function* watchAddComment() {
+  yield takeLatest(ADD_COMMENT_REQUEST, addComment); // 게시글은 여러번 클릭해도 1번만 게시되어야 한다.
+}
+
 export default function* postSaga() {
-  yield all([fork(watchAddPost)]);
+  yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
