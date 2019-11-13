3b285068839671d9899e52c59ad335027015fb59
한혜경 <hyegyeong310@gmail.com>
2019-11-13 11:14:10 +0900

    [Add] propTypes isRequired 추가

--- ch1/front/pages/_app.js
+++ ch1/front/pages/_app.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { createStore, compose, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import Head from 'next/head';
-import withRedux from 'next-redux-wrapper';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Provider } from "react-redux";
+import { createStore, compose, applyMiddleware } from "redux";
+import createSagaMiddleware from "redux-saga";
+import Head from "next/head";
+import withRedux from "next-redux-wrapper";
+import PropTypes from "prop-types";
 
-import AppLayout from '../components/AppLayout';
-import reducer from '../reducers';
-import rootSage from '../sagas';
+import AppLayout from "../components/AppLayout";
+import reducer from "../reducers";
+import rootSage from "../sagas";
 
 // _app.js는 공동 레이아웃으로 사용됌.
 // root 담당
@@ -32,20 +32,20 @@ const NodeBird = ({ Component, store }) => {
 };
 
 NodeBird.propTypes = {
-  Component: PropTypes.elementType,
-  store: PropTypes.object
+  Component: PropTypes.elementType.isRequired,
+  store: PropTypes.object.isRequired
 };
 
 const configureStore = (initialState, options) => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
   const enhancer =
-    process.env.NODE_ENV === 'production'
+    process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares))
       : compose(
           applyMiddleware(...middlewares),
           !options.isServer &&
-            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
+            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
             ? window.__REDUX_DEVTOOLS_EXTENSION__()
             : f => f
         );
