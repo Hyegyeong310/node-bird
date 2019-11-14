34babd0328b69ddd216a9e7cd2d04015e7ac7c34
한혜경 <hyegyeong310@gmail.com>
2019-11-14 20:08:54 +0900

    [Add] env 변수 설정. gitignore에 .env 추가

---
+++ ch1/back/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const dotenv = require("dotenv");
+const passport = require("passport");
+const passportConfig = require("./passport");
+
+const db = require("./models");
+const userAPIRouter = require("./routes/user");
+const postAPIRouter = require("./routes/post");
+const postsAPIRouter = require("./routes/posts");
+
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+db.sequelize.sync();
+passportConfig();
+
+app.use(morgan("dev"));
+app.use(express.json()); // 데이터 처리
+app.use(express.urlencoded({ extended: true })); // form 처리
+app.use(cors());
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  expressSession({
+    resave: false, // 매번 세션 강제 저장
+    saveUninitialized: false, // 빈 값도 저장
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false // https를 사용할 때 true 사용
+    }
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session()); // 미들웨어 간 의존관계가 있을 시 순서가 중요!
+
+// api는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
+app.use(`/api/user`, userAPIRouter);
+app.use(`/api/post`, postAPIRouter);
+app.use(`/api/posts`, postsAPIRouter);
+
+app.listen(PORT, () =>
+  console.log(`server is running on http://localhost:${PORT}`)
+);
