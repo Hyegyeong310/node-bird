70603732ad4e77f4be37bfd92df37855aed11168
한혜경 <hyegyeong310@gmail.com>
2019-11-17 14:41:27 +0900

    [Add] front next server와 express 서버 연결

---
+++ ch1/front/server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const next = require("next");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const expressSession = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const dev = process.env.NODE_ENV !== "production";
+const prod = process.env.NODE_ENV === "production";
+const PORT = process.env.PORT;
+
+const app = next({
+  dev
+});
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+  server.use(morgan("dev"));
+  server.use(express.json());
+  server.use(express.urlencoded({ extended: true }));
+  server.use(cookieParser(process.env.COOKIE_SECRET));
+  server.use(
+    expressSession({
+      resave: false,
+      saveUninitialized: false,
+      secret: "",
+      cookie: {
+        httpOnly: true,
+        secure: false
+      }
+    })
+  );
+  // * -> 모든 get 요청을 처리
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(PORT, () => {
+    console.log(`next+express running on port ${PORT}`);
+  });
+});
